3b25c6c981e1f53f93997b04c87fec9f282dc275
nastar <an1413@hs.ac.kr>
2023-06-10 12:38:27 +0900

    Feat: 라우터셋팅

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import GlobalStyles from "./GlobalStyles";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./compoents/login/Login.jsx";
+import Signup from "./compoents/signup/Signup.jsx";
+import Weather from "./compoents/weather/Weather.jsx";
 
 export default function App() {
   return (
     <>
       <GlobalStyles />
-      <div>Appdd</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="Signup" element={<Signup />} />
+          <Route path="Weather" element={<Weather />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
